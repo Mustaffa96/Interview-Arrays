c6d5ebf83d9fd4afab0bfed3b82caa98e3cedec6
Ahmad Mustaffa <49057200+Mustaffa96@users.noreply.github.com>
2021-11-26 21:10:14 +0800

    Create Majority Element.cpp

---
+++ Majority Element.cpp
@@ -0,0 +1,104 @@
+/*
+Given an array A of N elements. Find the majority element in the array. A majority element in an array A of size N is an element that appears more than N/2 times in the array.
+ 
+
+Example 1:
+
+Input:
+N = 3 
+A[] = {1,2,3} 
+Output:
+-1
+Explanation:
+Since, each element in 
+{1,2,3} appears only once so there 
+is no majority element.
+Example 2:
+
+Input:
+N = 5 
+A[] = {3,1,3,3,2} 
+Output:
+3
+Explanation:
+Since, 3 is present more
+than N/2 times, so it is 
+the majority element.
+
+Your Task:
+The task is to complete the function majorityElement() which returns the majority element in the array. If no majority exists, return -1.
+ 
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+ 
+
+Constraints:
+1 ≤ N ≤ 107
+0 ≤ Ai ≤ 106
+*/
+
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+// Function to find majority element in the array
+// a: input array
+// size: size of input array
+int majorityElement(int a[], int size)
+{
+        
+      if(size==1){
+           return a[0];
+       }
+       int count=0;
+       int temp=0;
+     
+       int ab=(int)(size/2);
+       for(int i=0;i<size;i++){//3 1 3 3 2
+           if(count==0){
+               temp=a[i];//3 3
+           }if(temp==a[i]){
+               count++;}//2
+               else{
+               count--;}//1
+           }
+           for(int i=0;i<size;i++)
+           {
+               if(a[i]==temp)
+               count++;
+           }
+       if(count>ab)
+       return temp;
+       else
+       return -1;
+        
+}
+
+// { Driver Code Starts.
+
+int main(){
+
+    int t;
+    scanf("%d", &t);
+
+    while(t--){
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            scanf("%d", &arr[i]);
+        }
+        printf("%d\n", majorityElement(arr, n));
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
